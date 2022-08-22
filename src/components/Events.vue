513132ecd90008280c36e825abafa19b7e968c5f
Temur Komilov <temurkomilov479@gmail.com>
2022-08-22 21:52:37 +0500

    Events

---
+++ src/components/Events.vue
@@ -0,0 +1,33 @@
+<!-- eslint-disable -->
+<template>
+  <div class="container">
+    <div class="events">
+      <h2 class="events-title">
+        <p class="d-inline m-0">Globally</p> Accepted
+      </h2>
+      <p class="m-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum <br> has
+        been the
+        industry's
+        standard dummy text ever since the 1500s, when an unknown <br> printer took a galley of type and scrambled it to
+        make a
+        type specimen book.
+      </p>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="cart box domestic">
+            <h2>For Domestic</h2>
+            <a href="#">Start trip taday</a>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="cart box international">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
